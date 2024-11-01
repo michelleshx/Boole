5b702320368f2aece43a9738bbe09da7de193f87
michelle <michelleshx462@gmail.com>
2024-11-01 13:25:29 -0400

    remove username

--- src/hooks/useSubmission.ts
+++ src/hooks/useSubmission.ts
@@ -3,23 +3,6 @@
 import { useState } from "react";
 import axios from "axios";
 
-// API URL and Authentication header token
-const apiUrl = "https://markus.student.cs.uwaterloo.ca/markus_cs116ae_f/api/";
-const authHeader = {
-  Authorization: process.env.REACT_APP_MARKUS_AUTH,
-  ContentType: "application/json",
-};
-
-const getUsername = (text: string) => {
-  // #u userName
-  const regex = /#u\s*(\S+)/g;
-  let match;
-  let results = [];
-
-  while ((match = regex.exec(text)) !== null) results.push(match[1]);
-  return results.toString();
-};
-
 const getAssignmentNumber = (text: string) => {
   // #a 01
   const regex = /#a\s*(\S+)/g;
@@ -46,62 +29,54 @@ const useSubmission = (onVerify: (feedback: string) => void) => {
   const [submitting, setSubmitting] = useState(false);
   const [submittedValue, setSubmittedValue] = useState<string | null>(null);
 
-  const getAssignments = async () => {
-    try {
-      const response = await axios.get("/assignments.json", {
-        headers: authHeader,
-      });
-      console.log("Getting assignments in MarkUs:", response.status);
-      response.data.forEach(
-        (assn: {
-          id: number;
-          short_identifier: string;
-          description: string;
-        }) => {
-          console.log(assn.id, assn.short_identifier, assn.description);
-        }
-      );
-    } catch (error) {
-      console.error("Error getting assignments:", error);
-    }
-  };
+  // const getAssignments = async () => {
+  //   try {
+  //     const response = await axios.get("/cgi-bin/markus_get_file.py", {});
+  //     console.log("Getting assignments in MarkUs:", response.status);
+  //     response.data.forEach(
+  //       (assn: {
+  //         id: number;
+  //         short_identifier: string;
+  //         description: string;
+  //       }) => {
+  //         console.log(assn.id, assn.short_identifier, assn.description);
+  //       }
+  //     );
+  //   } catch (error) {
+  //     console.error("Error getting assignments:", error);
+  //   }
+  // };
 
   const submit = (valueToValidate: string) => {
     setSubmitting(true);
 
     gtag("event", "submit");
 
-    const username = getUsername(valueToValidate);
     const assignmentNum = getAssignmentNumber(valueToValidate);
     const questionNum = getQuestionNumber(valueToValidate);
 
     const data = {
-      student_username: username, // The username of the student
-      submit_action: "upload", // Upload to add or update a file
+      assignment_id: assignmentNum,
       files: [
         {
           filename: `a${assignmentNum}q${questionNum}.grg`, // Name of the file to submit
-          encoding: "text", // We're submitting a text file
-          content: valueToValidate, // The contents of the file
+          encoding: "text",
+          content: valueToValidate,
         },
       ],
     };
 
     axios
-      .post(
-        `/assignments/${assignmentNum}/student_submissions/submit.json`,
-        data,
-        {
-          headers: authHeader,
-        }
-      )
-      .then(() => {
+      .post(`/george/ask-george/cgi-bin/markus_submit.cgi`, data)
+      .then((response) => {
+        console.log(response.data);
         setSubmittedValue(valueToValidate);
         onVerify(
           `Successfully submitted a${assignmentNum}q${questionNum}.grg to Markus!`
         );
       })
-      .catch(() => {
+      .catch((e) => {
+        console.error(e);
         onVerify("Error submitting assignment to Markus!");
       })
       .finally(() => {
