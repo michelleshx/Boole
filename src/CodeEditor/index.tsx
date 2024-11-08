81289d5d351ae35673ce4c8e2f7341accd571823
michelle <michelleshx462@gmail.com>
2024-11-07 21:19:19 -0500

    fix save keybindings

--- src/CodeEditor/index.tsx
+++ src/CodeEditor/index.tsx
@@ -1,8 +1,8 @@
 import { FileContext } from "../context/FileContext";
-import { LanguageServerContext } from '../context/LanguageServerContext'
-import React, { useEffect, useContext } from 'react';
-import Editor from '@monaco-editor/react';
-import {registerGeorge} from './monaco-george';
+import { LanguageServerContext } from "../context/LanguageServerContext";
+import React, { useEffect, useContext } from "react";
+import Editor from "@monaco-editor/react";
+import { registerGeorge } from "./monaco-george";
 import { BeforeMount, Monaco, OnMount } from "@monaco-editor/react";
 import * as monaco from "monaco-editor";
 
@@ -12,14 +12,12 @@ interface EditorProps {
   autocomplete: boolean;
 }
 
-const CodeEditor = ({
-  isDarkMode,
-  onCheck,
-  autocomplete,
-}: EditorProps) => {
+const CodeEditor = ({ isDarkMode, onCheck, autocomplete }: EditorProps) => {
   const { value, setValue, openFile } = useContext(FileContext);
-  const { editorRef, monacoRef, sendDidChangeMessage  } = useContext(LanguageServerContext);
-  
+  const { editorRef, monacoRef, sendDidChangeMessage } = useContext(
+    LanguageServerContext
+  );
+
   const handleEditorWillMount: BeforeMount = (monaco) => {
     // Remove all keybindings we want to handle globally
     monaco.editor.addKeybindingRules([
@@ -42,7 +40,7 @@ const CodeEditor = ({
       {
         keybinding: monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyU,
         command: null,
-      }
+      },
     ]);
   };
 
@@ -52,63 +50,72 @@ const CodeEditor = ({
       if (openFile !== null) openFile.set(value);
     }
 
-	if(editorRef.current?.getModel()) {
-	  sendDidChangeMessage(editorRef.current.getModel()!);
-	}
-
+    if (editorRef.current?.getModel()) {
+      sendDidChangeMessage(editorRef.current.getModel()!);
+    }
   };
 
-  function setEditorTheme(){
+  function setEditorTheme() {
     const rootStyles = getComputedStyle(document.body);
-    const background1 = rootStyles.getPropertyValue('--background-1').trim();
-    const background2 = rootStyles.getPropertyValue('--background-2').trim();
-    const textColourEditor = rootStyles.getPropertyValue('--text-color-editor').trim();
-    const textColourEditor2 = rootStyles.getPropertyValue('--text-color-editor-secondary').trim();
-    const textColourEditor3 = rootStyles.getPropertyValue('--text-color-editor-tertiary').trim();
-    const backgroundHighlighted = rootStyles.getPropertyValue('--background-highlighted').trim();
-    const textHighlighted = rootStyles.getPropertyValue('--text-color-tertiary').trim();
-    const cursorColour = rootStyles.getPropertyValue('--cursor').trim();
+    const background1 = rootStyles.getPropertyValue("--background-1").trim();
+    const background2 = rootStyles.getPropertyValue("--background-2").trim();
+    const textColourEditor = rootStyles
+      .getPropertyValue("--text-color-editor")
+      .trim();
+    const textColourEditor2 = rootStyles
+      .getPropertyValue("--text-color-editor-secondary")
+      .trim();
+    const textColourEditor3 = rootStyles
+      .getPropertyValue("--text-color-editor-tertiary")
+      .trim();
+    const backgroundHighlighted = rootStyles
+      .getPropertyValue("--background-highlighted")
+      .trim();
+    const textHighlighted = rootStyles
+      .getPropertyValue("--text-color-tertiary")
+      .trim();
+    const cursorColour = rootStyles.getPropertyValue("--cursor").trim();
 
-    var baseTheme = 'vs-dark'
-    if (isDarkMode){
-      baseTheme = 'vs-dark'
+    var baseTheme = "vs-dark";
+    if (isDarkMode) {
+      baseTheme = "vs-dark";
     } else {
-      baseTheme = 'vs'
+      baseTheme = "vs";
     }
 
     if (monacoRef.current) {
-      monacoRef.current.editor.defineTheme('george-custom-theme', {
-        base: 'hc-light',
+      monacoRef.current.editor.defineTheme("george-custom-theme", {
+        base: "hc-light",
         inherit: false,
         rules: [
-          { token: 'comment', foreground: '6272a4' },
-          { token: 'keyword', foreground: 'ff79c6' },
-          { token: 'type', foreground: '66d9ef' },
-          { token: 'constant', foreground: textColourEditor2 },
-          { token: 'identifier', foreground: textColourEditor3 }, // Generic identifiers (e.g., variable names)
+          { token: "comment", foreground: "6272a4" },
+          { token: "keyword", foreground: "ff79c6" },
+          { token: "type", foreground: "66d9ef" },
+          { token: "constant", foreground: textColourEditor2 },
+          { token: "identifier", foreground: textColourEditor3 }, // Generic identifiers (e.g., variable names)
           {
-            token: 'other', // Token type to style
-            foreground: 'FFFFFF', // Example: White text
-            background: 'FF0000', // Not natively supported in `rules`, needs workaround
+            token: "other", // Token type to style
+            foreground: "FFFFFF", // Example: White text
+            background: "FF0000", // Not natively supported in `rules`, needs workaround
           },
         ],
         colors: {
-          'editor.background': background1,
-          'editor.foreground': textColourEditor,
-          'editorCursor.foreground': cursorColour,
-          'editor.wordHighlightBackground': textHighlighted,
-          'editor.lineHighlightBorder': background1,
-      
+          "editor.background": background1,
+          "editor.foreground": textColourEditor,
+          "editorCursor.foreground": cursorColour,
+          "editor.wordHighlightBackground": textHighlighted,
+          "editor.lineHighlightBorder": background1,
+
           // Add line number coloring
-          'editorLineNumber.foreground': '#6272a4', // Customize this to your desired color
-      
+          "editorLineNumber.foreground": "#6272a4", // Customize this to your desired color
+
           // cmd/ctrl F menu
-          'editorWidget.background': background1,
-          'editorWidget.foreground': textColourEditor, // Text color
-          'editorWidget.resizeBorder': '#6272a4', // Resize handle color
-          'input.background': background2, // Text field background
-          'input.foreground': textColourEditor, // Text field text color
-          'input.border': '#44475a', // Text field border color
+          "editorWidget.background": background1,
+          "editorWidget.foreground": textColourEditor, // Text color
+          "editorWidget.resizeBorder": "#6272a4", // Resize handle color
+          "input.background": background2, // Text field background
+          "input.foreground": textColourEditor, // Text field text color
+          "input.border": "#44475a", // Text field border color
         },
       });
     }
@@ -118,36 +125,35 @@ const CodeEditor = ({
     editor: monaco.editor.IStandaloneCodeEditor,
     monaco: Monaco
   ) => {
-
     editorRef.current = editor;
     monacoRef.current = monaco;
 
-    setEditorTheme()
+    setEditorTheme();
 
-    editor.addCommand(
-      monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter,
-      () => {
-        const editorContent = editor.getValue();
-        onCheck(editorContent);
-      }
-    );    
-    
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter, () => {
+      const editorContent = editor.getValue();
+      onCheck(editorContent);
+    });
+
     // Apply the custom theme
-    monaco.editor.setTheme('george-custom-theme');
+    monaco.editor.setTheme("george-custom-theme");
     registerGeorge(editor, monaco);
     monaco.editor.setModelLanguage(editor.getModel()!, "george");
-
-  }
+  };
 
   useEffect(() => {
     if (monacoRef.current) {
-      
-      setEditorTheme()
+      setEditorTheme();
 
       // Re-apply the custom theme
-      monacoRef.current.editor.setTheme('george-custom-theme');
+      monacoRef.current.editor.setTheme("george-custom-theme");
     }
   }, [isDarkMode]);
+
+  useEffect(() => {
+    const storedMode = localStorage.getItem("keybinding");
+    storedMode && setKeybinding(JSON.parse(storedMode));
+  }, [keybinding, setKeybinding]);
 
   return (
     <Editor
