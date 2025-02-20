35c26f7c5c82316d4aafe3f5fa870e9f8171ec7c
michelle <michelleshx462@gmail.com>
2025-02-20 15:35:46 -0500

    get file type for debugging panel

--- src/context/FileContext.tsx
+++ src/context/FileContext.tsx
@@ -8,6 +8,7 @@ type FileContextType = {
   setOpenFile: Dispatch<SetStateAction<File>>;
   fileType: FileType;
   setFileType: Dispatch<SetStateAction<FileType>>;
+  getFileType: (val: string) => FileType;
 };
 
 export const FileContext = createContext<FileContextType>(
@@ -19,12 +20,40 @@ const FileProvider: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   const [value, setValue] = useState<string>(""); // current value in text editor
   const [openFile, setOpenFile] = useState<File>({} as File); // which file is open in editor
-  // TODO set file type when loading file?
   const [fileType, setFileType] = useState<FileType>(FileType.PREDTYPE);
+
+  const checkToFileType: { [key: string]: FileType } = {
+    "#check CE": FileType.COUNTEREXAMPLE,
+    "#check Z": FileType.Z,
+    "#check PREDTYPE": FileType.PREDTYPE,
+    "#check PRED": FileType.PRED,
+    "#check TP": FileType.TP,
+    "#check ST": FileType.ST,
+    "#check PROP": FileType.PROP,
+    "#check PC": FileType.PC,
+    "#check ND": FileType.ND,
+  };
+
+  const getFileType = (value: string): FileType => {
+    for (const key in checkToFileType) {
+      if (value.includes(key)) {
+        return checkToFileType[key];
+      }
+    }
+    return FileType.NONE; // Default case
+  };
 
   return (
     <FileContext.Provider
-      value={{ value, setValue, openFile, setOpenFile, fileType, setFileType }}
+      value={{
+        value,
+        setValue,
+        openFile,
+        setOpenFile,
+        fileType,
+        setFileType,
+        getFileType,
+      }}
     >
       {children}
     </FileContext.Provider>
