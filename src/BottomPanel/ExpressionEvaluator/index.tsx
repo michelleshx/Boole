c7a025d7a090530ad5a01789ad8f605697d43e49
michelle <michelleshx462@gmail.com>
2025-02-20 11:26:12 -0500

    basic eval and  restyle

--- src/BottomPanel/ExpressionEvaluator/index.tsx
+++ src/BottomPanel/ExpressionEvaluator/index.tsx
@@ -1,19 +1,42 @@
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { useState, useContext } from "react";
 import styles from "./ExpressionEvaluator.module.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
+import useEvaluator from "../../hooks/useEvaluator";
+import { FileContext } from "../../context/FileContext";
+import { Button, Loading } from "../../components";
 
 const ExpressionEvaluator = () => {
   const [expression, setExpression] = useState("");
+  const [error, setError] = useState(false);
   const [result, setResult] = useState("");
+  const { value } = useContext(FileContext);
 
-  const evaluateExpression = () => {
-    setResult("result!"); // TODO
+  const { evaluate, evaluating } = useEvaluator((feedback: string) => {
+    setError(feedback.includes("error"));
+    setResult(feedback);
+  });
+
+  const handleClear = () => {
+    setExpression("");
+    setResult("");
+    setError(false);
+  };
+
+  const handleEvaluate = () => {
+    if (!expression.trim()) {
+      setResult("Oops, please enter an expression!");
+      setError(true);
+      return;
+    }
+    evaluate(value?.concat(expression) || expression);
   };
 
   return (
     <div className={styles.expressionEvaluator}>
-      <label className={styles.label}>Expression</label>
+      <label className={styles.label}>Expression Evaluator</label>
       <div className={styles.row}>
         <input
           className={styles.input}
@@ -21,17 +44,28 @@ const ExpressionEvaluator = () => {
           onChange={(e) => setExpression(e.target.value)}
           value={expression}
         />
-        <button
-          className={styles.iconButton}
-          onClick={() => evaluateExpression()}
-          aria-label="Evaluate expression"
-          title="Evaluate expression"
+        {expression && (
+          <button
+            className={styles.clearButton}
+            onClick={handleClear}
+            title="Clear Input"
+          >
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        )}
+        <Button
+          text="Evaluate"
+          title="Evaluate"
+          variant="primary"
+          disabled={evaluating}
+          onClick={handleEvaluate}
         >
-          <FontAwesomeIcon icon={faPlay} style={{ marginLeft: "2px" }} />
-        </button>
+          {evaluating && <Loading />}
+        </Button>
       </div>
-      <label className={styles.label}>Result</label>
-      <input className={styles.result} value={result} />
+      <p className={`${styles.result} ${error ? styles.error : ""}`}>
+        {result}
+      </p>
     </div>
   );
 };
